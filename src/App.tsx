fc6f9c5546b2c9e9db45c17224a5ea40a7c069ef
SerhiiKI13 <kugai.sirozha@gmail.com>
2025-03-23 17:10:11 +0100

    finish task #gi01MZ6v

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
 
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
 function App() {
 
 
     return (
         <>
+            <div className='App'>
+                <ul>
+                    {coursesTitleArray.map((course, index) => <li key={index}>{course}</li>)}
 
+                </ul>
+            </div>
         </>
     )
 }
